169725cbbd32748f9dd6ac505e4a7e67221b373c
binsabit <yerdaulet.seidizhappar@gmail.com>
2023-06-14 22:37:40 +0600

    storage interface DONE

--- internal/data/storage.go
+++ internal/data/storage.go
@@ -3,6 +3,6 @@ package data
 type Storage interface {
 	CreateAccount(*Account) error
 	DeleteAccount(int) error
-	UpdateAccount(*Account) error
+	UpdateAccount(int, *Account) error
 	GetAccountByID(ID int) (*Account, error)
 }
